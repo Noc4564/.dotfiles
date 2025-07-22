4dfc4c73c02e4de96d2528e9fff798aa8bfa6b74
Jacob <jacobsponsel4564@gmail.com>
2025-07-22 11:46:13 -0700

    added powermenu script and waybar slider configs for brightness and sound

---
+++ .config/scripts/waybar/powermenu.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+DISPLAY_NAMES=(Lock Logout Shutdown Restart Shutdown)
+COMMANDS=('loginctl lock-session' 'hyprctl dispatch exit' 'systemctl poweroff' 'systemctl reboot' 'systemctl suspend')
+ICON_PATHS=(
+   # Lock
+  󰍃 # Logout
+   # Shutdown
+  󰜉 # Restart
+  󰒲 # Sleep
+)
+
+MENU_ITEMS=()
+for i in "${!DISPLAY_NAMES[@]}"; do
+  MENU_ITEMS+=("img:${ICON_PATHS[i]}:text:${DISPLAY_NAMES[i]}")
+done
+
+CHOICE=$(printf '%s\n' "${MENU_ITEMS[@]}" | wofi --show dmenu)
+
+# Extract label from `text:...`
+SELECTED_NAME="${CHOICE#*:text:}"
+
+# Match selection and run command
+for i in "${!DISPLAY_NAMES[@]}"; do
+  if [[ "${DISPLAY_NAMES[i]}" == "$SELECTED_NAME" ]]; then
+    eval "${COMMANDS[i]}"
+    break
+  fi
+done
+
