65f8df1feab79f821c887165af21411851f673b2
Jacob <jacobsponsel4564@gmail.com>
2025-07-28 15:43:07 -0700

    Changed values for auto lock

--- .config/swaylock/swaylock-idle.sh
+++ .config/swaylock/swaylock-idle.sh
@@ -1,5 +1,5 @@
 swayidle -w \
-  timeout 300 'swaylock -e -f -i ${HOME}/.config/swaylock/background.jpeg' \
-  timeout 600 'swaymsg "output \* dpms off"' \
-  timeout 660 'systemctl suspend' \
+  timeout 500 'swaylock -e -f -i ${HOME}/.config/swaylock/background.jpeg' \
+  timeout 1000 'swaymsg "output \* dpms off"' \
+  timeout 2000 'systemctl suspend' \
 	      resume 'swaymsg "output \* dpms on"'
