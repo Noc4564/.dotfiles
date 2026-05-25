59d54eb895167281fcedaa712b75dcdd9bb7d8a8
Jacob <jacobsponsel4564@gmail.com>
2026-05-25 12:27:27 -0700

    nvim now tabs 2 spaces

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,10 @@
+-- enable clipboard
 vim.opt.clipboard = "unnamedplus"
+-- enable line numbers
 vim.opt.number = true
+-- change tabs to 2 spaces
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
+
