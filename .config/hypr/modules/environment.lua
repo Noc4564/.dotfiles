d5541071cee40ee17136279b2ec37198b0224fd9
Jacob <jacobsponsel4564@gmail.com>
2026-05-27 23:55:15 -0700

    playing around with animations

--- .config/hypr/modules/environment.lua
+++ .config/hypr/modules/environment.lua
@@ -1,3 +1,4 @@
+-- Layout of the environment
 hl.config({
   general = {
     border_size = 3,
@@ -15,3 +16,44 @@ hl.config({
     inactive_opacity = 0.95,
   }
 })
+
+-- Animations
+-- Curves
+hl.curve("whiplash", {
+  type = "bezier",
+  points = { {0.24, -0.49}, {0.62, 1.16} }
+})
+hl.curve("whiplash2", {
+  type = "bezier",
+  points = { {0.73, -1}, {0.2, 1.45} }
+})
+hl.curve("smooth", {
+  type = "bezier",
+  points = { {0.17, 0.36}, {0.57, 0.84} }
+})
+
+-- Applying the animations
+-- window movement
+hl.animation ({
+  leaf = "windows",
+  enabled = true,
+  speed = 6,
+  bezier = "whiplash",
+  style = "slide"
+})
+-- workspace
+hl.animation ({
+  leaf = "workspaces",
+  enabled = true,
+  speed = 8,
+  bezier = "whiplash2",
+  style = "slidefadevert"
+})
+-- fades
+hl.animation ({
+  leaf = "fadePopups",
+  enabled = true,
+  bezier = "smooth",
+  speed = 10
+})
+
